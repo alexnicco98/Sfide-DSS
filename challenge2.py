a90b9e29bb369a00a6987f5a800ebe803d10ee2e
Alessandro Niccolini <a.niccolini@protonmail.com>
2021-10-19 21:49:10 +0200

    i punti trovati sono stati salvati in un file kml

--- challenge2.py
+++ challenge2.py
@@ -3,6 +3,8 @@ from PIL import Image
 from PIL.ExifTags import TAGS
 import os
 from os.path import join
+import sys
+from gensim.utils import tokenize
 
 '''def decimal_coords(coords, ref):
     decimal_degrees = coords[0] + coords[1] / 60 + coords[2] / 3600
@@ -40,6 +42,60 @@ from os.path import join
     if 'Latitude' in info and 'Longitude' in info:
         return [info['Latitude'], info['Longitude']]'''
 
+'''def dms_to_decimal(degrees, minutes, seconds, sign=' '):
+    """Convert degrees, minutes, seconds into decimal degrees.
+
+    >>> dms_to_decimal(10, 10, 10)
+    10.169444444444444
+    >>> dms_to_decimal(8, 9, 10, 'S')
+    -8.152777777777779
+    """
+    return (-1 if sign[0] in 'SWsw' else 1) * (
+        float(degrees)        +
+        float(minutes) / 60   +
+        float(seconds) / 3600
+    )
+
+
+def print_gps(image_file):
+    try:
+        metadata = pyexiv2.metadata.ImageMetadata(image_file)
+        metadata.read()
+        thumb = metadata.exif_thumbnail
+
+        try:
+            latitude = metadata.__getitem__("Exif.GPSInfo.GPSLatitude")
+            latitudeRef = metadata.__getitem__("Exif.GPSInfo.GPSLatitudeRef")
+            longitude = metadata.__getitem__("Exif.GPSInfo.GPSLongitude")
+            longitudeRef = metadata.__getitem__("Exif.GPSInfo.GPSLongitudeRef")
+
+            latitude = str(latitude).split("=")[1][1:-1].split(" ");
+            latitude = map(lambda f: str(float(Fraction(f))), latitude)
+            latitude = latitude[0] + u"\u00b0" + latitude[1] + "'" + latitude[2] + '"' + " " + str(latitudeRef).split("=")[1][1:-1]
+
+            longitude = str(longitude).split("=")[1][1:-1].split(" ");
+            longitude = map(lambda f: str(float(Fraction(f))), longitude)
+            longitude = longitude[0] + u"\u00b0" + longitude[1] + "'" + longitude[2] + '"' + " " + str(longitudeRef).split("=")[1][1:-1]
+
+            latitude_value = dms_to_decimal(*metadata.__getitem__("Exif.GPSInfo.GPSLatitude").value + [metadata.__getitem__("Exif.GPSInfo.GPSLatitudeRef").value]);
+            longitude_value = dms_to_decimal(*metadata.__getitem__("Exif.GPSInfo.GPSLongitude").value + [metadata.__getitem__("Exif.GPSInfo.GPSLongitudeRef").value]);
+
+            print("--- GPS ---")
+            print("Coordinates: " + latitude + ", " + longitude)
+            print("Coordinates: " + str(latitude_value) + ", " + str(longitude_value))
+            print("--- GPS ---")
+        except Exception as e:
+            print("No GPS Information!")
+            #print e
+
+        # Check for thumbnail
+        if(thumb.data == ""):
+            print("No thumbnail!")
+    except Exception as e:
+        print("Error processing image...")
+        print(e)'''
+
+
 # sistemare in base alla libreria pyexiv2
 def dms_to_dd(gps_coords, gps_coords_ref):
     d, m, s = gps_coords
@@ -59,16 +115,51 @@ def get_field(exif, field):
 
 
 def get_exif(fn):
+    '''path = bytes(fn, 'utf-8')
+    with open(path, 'rb') as f:
+        with pyexiv2.ImageData(f.read()) as img:
+            data = img.read_exif()
+            if not len(data) == 0:
+                ret = {}
+                for tag, value in data:
+                    decoded = TAGS.get(tag, tag)
+                    ret[decoded] = value
+                print(ret)'''
     img = pyexiv2.Image(fn)
     exif_info = img.read_exif()
     if not len(exif_info) == 0:
         print(exif_info)
-        #print(exif_info['Exif.Image.ImageWidth'])
-        print(dms_to_dd(exif_info["Exif.GPSInfo.GPSLatitude"], exif_info["Exif.GPSInfo.GPSLatitudeRef"]))
-        print(dms_to_dd(exif_info["Exif.GPSInfo.GPSLongitude"], exif_info["Exif.GPSInfo.GPSLongitudeRef"]))
-    # print(get_coordinates(exif_info))
-    # print(img.read_iptc())
-    # print(img.read_xmp())
+        a = list(tokenize(exif_info['Exif.GPSInfo.GPSLatitude']))
+        print(a)
+        # print(exif_info['Exif.Image.ImageWidth'])
+        '''
+        # Read the GPS info.
+        latref = exif_info['Exif.GPSInfo.GPSLatitudeRef']
+        print(exif_info['Exif.GPSInfo.GPSLatitude'])
+        print( float(exif_info['Exif.GPSInfo.GPSLatitude'][0]) / float(exif_info['Exif.GPSInfo.GPSLatitude'][2]))
+        print(float(exif_info['Exif.GPSInfo.GPSLatitude'][4]) / float(exif_info['Exif.GPSInfo.GPSLatitude'][6]))
+        #lat = [float(exif_info['Exif.GPSInfo.GPSLatitude'][0]), float(exif_info['Exif.GPSInfo.GPSLatitude'][3]), float(exif_info['Exif.GPSInfo.GPSLatitude'][6])]
+        #print(lat)
+        lonref = exif_info['Exif.GPSInfo.GPSLongitudeRef']
+        lon = float(exif_info['Exif.GPSInfo.GPSLongitude'])
+
+
+        # Convert the latitude and longitude to signed floating point values.
+        latitude = float(lat[0]) + float(lat[1]) / 60 + float(lat[2]) / 3600
+        longitude = float(lon[0]) + float(lon[1]) / 60 + float(lon[2]) / 3600
+        if latref == 'S': latitude = -latitude
+        if lonref == 'W': longitude = -longitude
+
+        # Construct the Google Maps query and open it.
+        query = "http://maps.google.com/maps?q=loc:%.6f,%.6f" % (latitude, longitude)
+        subprocess.call(['open', query])'''
+
+
+# print(dms_to_dd(exif_info["Exif.GPSInfo.GPSLatitude"], exif_info["Exif.GPSInfo.GPSLatitudeRef"]))
+# print(dms_to_dd(exif_info["Exif.GPSInfo.GPSLongitude"], exif_info["Exif.GPSInfo.GPSLongitudeRef"]))
+# print(get_coordinates(exif_info))
+# print(img.read_iptc())
+# print(img.read_xmp())
 
 
 def is_jpg(filename):
@@ -90,8 +181,12 @@ def search_jpg(path):
 
 mypath = "F:"
 file_jpg = search_jpg(mypath)
-print(file_jpg)
+# print(file_jpg)
 for i in file_jpg:
     get_exif(i)
     # image_coordinates(i)
-# get_coordinates(exif['GPSInfo'])
+    # get_coordinates(exif['GPSInfo'])
+
+    ''''# Get the data from image file and return a dictionary
+    data = gpsphoto.getGPSData(i)
+    print(data['Latitude'], data['Longitude'])'''
